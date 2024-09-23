0d8fec027601dc4d2513f0195c2c71022197ee0e
zulfianaauliasyafa <zulfianaauliasyafa@gmail.com>
2024-09-23 20:20:51 +0700

    second commit

--- app/src/main/java/com/example/tugas4_recyclerview/MainActivity.kt
+++ app/src/main/java/com/example/tugas4_recyclerview/MainActivity.kt
@@ -1,20 +1,76 @@
 package com.example.tugas4_recyclerview
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.tugas4_recyclerview.model.UserData
+import com.example.tugas4_recyclerview.view.UserAdapter
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var addBtn:FloatingActionButton
+    private lateinit var recy:RecyclerView
+    private lateinit var userList: ArrayList<UserData>
+    private lateinit var userAdapter: UserAdapter
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        userList = ArrayList()
+        addBtn = findViewById(R.id.addingBtn)
+        recy = findViewById(R.id.mRecycler)
+        userAdapter = UserAdapter(this,userList)
+        recy.layoutManager = LinearLayoutManager(this)
+        recy.adapter = userAdapter
+        addBtn.setOnClickListener{ addInfo() }
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private fun addInfo() {
+        val inflter = LayoutInflater.from(this)
+        val v = inflter.inflate(R.layout.add_item,null)
+
+        val userName = v.findViewById<EditText>(R.id.userName)
+        val userNo = v.findViewById<EditText>(R.id.userNo)
+
+        val addDialog = AlertDialog.Builder(this)
+
+        addDialog.setView(v)
+        addDialog.setPositiveButton("Ok"){
+            dialog,_->
+            val names = userName.text.toString()
+            val number = userNo.text.toString()
+            userList.add(UserData("Nama: $names", "NIM : $number"))
+            userAdapter.notifyDataSetChanged()
+            Toast.makeText(this, "Adding User Information Success", Toast.LENGTH_SHORT).show()
+            dialog.dismiss()
+        }
+        addDialog.setNegativeButton("Cancel"){
+            dialog,_->
+            dialog.dismiss()
+            Toast.makeText(this, "Cancel", Toast.LENGTH_SHORT).show()
+
+        }
+        addDialog.create()
+        addDialog.show()
+
     }
 }
---
+++ app/src/main/java/com/example/tugas4_recyclerview/model/UserData.kt
@@ -0,0 +1,6 @@
+package com.example.tugas4_recyclerview.model
+
+data class UserData (
+    val userName:String,
+    val userMb: String
+)
